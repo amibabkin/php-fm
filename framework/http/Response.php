bfe38be1d2e8b47df0b0714b7573da6bfab062a2
Andrey Babkin <amibabkin@gmail.com>
2024-03-16 19:27:56 +0700

    feat: create response class

---
+++ framework/http/Response.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Somecode\Framework\http;
+
+class Response
+{
+  function __construct(
+    private mixed $content,
+    private int $statusCode = 200,
+    private array $headers = [],
+  ) {
+  }
+
+  public function send()
+  {
+    echo $this->content;
+  }
+}

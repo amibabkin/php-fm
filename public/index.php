d42c03dd4d9d82fa597a50772d6fda2adb4e25f9
Andrey Babkin <amibabkin@gmail.com>
2024-03-16 19:28:39 +0700

    feat: use response method in front controller

--- public/index.php
+++ public/index.php
@@ -3,7 +3,14 @@
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use Somecode\Framework\http\Request;
+use Somecode\Framework\http\Response;
 
 $request = Request::createFromGlobals();
+
+$clientContent = '<h1>Hello</h1>';
+
+$response = new Response($clientContent, 200, []);
+
+$response->send();
 
 dd($request);

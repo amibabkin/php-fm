6eb32ebd2f8e0479802f55372b978f01e31145bd
Andrey Babkin <amibabkin@gmail.com>
2024-03-16 19:47:19 +0700

    change: use kernel in front controller

--- public/index.php
+++ public/index.php
@@ -4,13 +4,18 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use Somecode\Framework\http\Request;
 use Somecode\Framework\http\Response;
+use Somecode\Framework\http\Kernel;
 
 $request = Request::createFromGlobals();
 
-$clientContent = '<h1>Hello</h1>';
+// $clientContent = '<h1>Hello</h1>';
 
-$response = new Response($clientContent, 200, []);
+// $response = new Response($clientContent, 200, []);
+
+// $response->send();
+
+$kernel = new Kernel();
+
+$response = $kernel->handle($request);
 
 $response->send();
-
-dd($request);
